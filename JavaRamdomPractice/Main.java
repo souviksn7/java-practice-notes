2ba4cbd5b6cc541f185080f9d711744dc5729322
Souvik Nandi <souviknandi235@gmail.com>
2025-04-19 15:55:59 +0530

    added random practice

---
+++ JavaRamdomPractice/Main.java
@@ -0,0 +1,85 @@
+package JavaRamdomPractice;
+
+import javax.swing.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]){
+//        Print<Integer> print = new Print<Integer>();
+//
+//        print.setValue(10);
+//        System.out.println(print.getValue());
+//
+//        System.out.println(print.getClass());
+//        System.out.println(Main.class);
+
+//        Car[] carArray = new Car[3];
+//        carArray[0] = new Car("Mahindra TUV", "AUV");
+//        carArray[1] = new Car("Alto 300", "small");
+//        carArray[2] = new Car("Creta", "Sedan");
+//
+//        Arrays.sort(carArray, (Car a, Car b) -> b.carType.compareTo(a.carType));
+//        for(Car car: carArray){
+//            System.out.println(car.carName + " " + car.carType);
+//        }
+
+//        ArrayList<Car> carList = new ArrayList<>();
+//        carList.add(new Car("Mahindra", "SUV"));
+//        carList.add(new Car("Alto", "Maruti"));
+//        carList.add(new Car("Creta", "BMW"));
+
+//        Collections.sort(carList, (Car a, Car b) -> a.carName.compareTo(b.carName));
+//        Collections.sort(carList, new CarComparator());
+//        carList.forEach((Car car) -> System.out.println(car.carName + " " + car.carType));
+
+//        ArrayList<Integer> list = new ArrayList<>();
+//        list.add(0,2);
+//        list.add(1,4);
+//        list.add(2,3);
+//        for(int val: list){
+//            System.out.println(val);
+//        }
+
+//        HashMap<Integer, String> map = new HashMap<>();
+//        map.put(1,"one");
+//        map.put(null, "null--");
+//        map.put(2,"two");
+//        map.put(3,"three");
+//        map.put(null, null);
+
+//        for(Integer key: map.keySet()){
+//            System.out.println(key + " " + map.get(key));
+//        }
+
+//        for(Map.Entry<Integer, String> entryMap: map.entrySet()){
+//            System.out.println(entryMap.getKey());
+//            System.out.println(entryMap.getValue());
+//        }
+
+//        TreeMap<Integer, String> map = new TreeMap<>((Integer key1, Integer key2) -> key2-key1);
+//        TreeMap<Integer, String> map = new TreeMap<>();
+//
+//
+//        map.put(2, "lol");
+//        map.put(1, "pop");
+//        map.put(3, "pull");
+//
+//        for(Map.Entry<Integer, String> entryMap : map.entrySet()){
+//            System.out.println(entryMap.getKey() + " " + entryMap.getValue());
+//        }
+
+//        System.out.println(Thread.currentThread().getName());
+//        System.out.println("Main thread is running " + " " + Thread.currentThread().getName());
+//        MultiThreadingLearning multiThreadingLearning = new MultiThreadingLearning();
+//        Thread thread = new Thread(multiThreadingLearning);
+//        thread.start();
+//        System.out.println("End of Main thread " + Thread.currentThread().getName());
+
+//        System.out.println(Thread.currentThread().getName());
+//        ThreadClass threadClass = new ThreadClass();
+//        threadClass.start();
+//        System.out.println("End of Main thread " + Thread.currentThread().getName());
+
+    }
+}
+
